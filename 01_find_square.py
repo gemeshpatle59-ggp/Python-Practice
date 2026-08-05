711418220ede71494e93935f3af903543e169810
gemeshpatle59-ggp <gemeshpatle59@gmail.com>
2026-08-05 16:27:19 +0530

    Add program to find the square of a number
    A simple Python program that calculates the square of a user-entered number.

---
+++ 01_find_square.py
@@ -0,0 +1,18 @@
+# WAP to find the square  of any number..
+
+print("\n==============================")
+print("-------SQUARE OF NUMBER-------")
+print("==============================")
+
+
+user = float(input("Enter a number here : "))
+
+square = (
+    user**2     
+)                     # '**' is use as power of number. ex (n**2) is n power 2
+
+print(f""
+      f"\nthe square of no.'{user}' is ==' {square} '=="
+      )
+
+
