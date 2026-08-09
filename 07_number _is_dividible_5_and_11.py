5f40f095f14fac84106eaa44b67edb29339cf982
gemeshpatle59-ggp <gemeshpatle59@gmail.com>
2026-08-09 18:32:52 +0530

    Create 07_number _is_dividible_5_and_11.py

---
+++ 07_number _is_dividible_5_and_11.py
@@ -0,0 +1,17 @@
+# Check whether a number is divisible by 5 and 11
+
+try:
+
+    n = float(input("Enter the number here.: "))
+
+    if n % 5 == 0 :
+        print(f"the number {n} is dividible by 5")
+
+    elif n % 11 == 0:
+        print(f"the number {n} is divisible by 11")    
+
+    else:
+        print("the number is not divisible by 5 or 11")
+        
+except ValueError:
+    print("Enter number porperly..")    
