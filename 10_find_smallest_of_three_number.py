6dd47ebf768e143db5f3a4b0db45d6f8952360fc
Gemesh patle <gemeshpatle59@gmail.com>
2026-08-10 20:23:38 +0530

    Create 10_find_smallest_of_three_number.py

---
+++ 10_find_smallest_of_three_number.py
@@ -0,0 +1,22 @@
+# Find the smallest of three numbers
+
+
+try:
+    n = float(input("Enter the 1st number here.: "))
+    m = float(input("Enter the 2st number here.: "))
+    o = float(input("Enter the 3st number here.: "))
+
+    if n < m and n < o:
+        print(f"{n} is smallest of three numbers.")
+      
+    elif m < n and m < o :
+        print(f"{m} is smallest of three numbers.")
+    
+    elif m == n and  n == o:
+        print(f"{n} {m} {o} all three are same.")
+    
+    else:
+        print(f"{o} is smallest of three numbers.")         
+
+except ValueError:
+    print("check the number properly..")    
