52306f0074243143bac5b6c8a3391f950700a06a
Gemesh patle <gemeshpatle59@gmail.com>
2026-08-12 20:54:04 +0530

    find_kilograms_to_gram
    A simple python program to find the kilogram into gram.

---
+++ 20_find_kilograms_to_gram.py
@@ -0,0 +1,10 @@
+# Convert kilograms to grams
+
+try:
+    n = float(input("Enter the kilogram here.: "))
+
+    gram = n * 1000
+    print(f"{n} kilogram is {gram:.1f} gram ")
+
+except ValueError:
+    print("enter proper length or breadth")
