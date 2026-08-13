7f2f6e136f84b08637b16b6f132bbaeda70564c1
Gemeshp <gemeshpatle831@gmail.com>
2026-08-13 22:46:39 +0530

    Add program 22 - Calculate compound interest

---
+++ 22_calculate_compound_interest.py
@@ -0,0 +1,17 @@
+# Calculate compound interest
+
+try:
+    n = int(input("Enter the principle amount here.: "))    
+    m = int(input("Enter the Rate of intrest here.: "))
+    o = int(input("Enter the Time period here.: "))
+
+    ci = n*(1+ m/100)**o - n
+
+    print(f"The compond intrest is {ci:.2f}")
+
+except ValueError:
+    print("Enter the correct value")
+
+
+
+    
