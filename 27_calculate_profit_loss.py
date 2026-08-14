1bf7fe20003e1c8d3e08da29a188d9fb69b5d5f4
Gemesh patle <gemeshpatle59@gmail.com>
2026-08-14 18:45:11 +0530

    Update program 27 - Calculate profit and loss
     A simple python program to calculate the profit and loss.

--- 27_calculate_profit_loss.py
+++ 27_calculate_profit_loss.py
@@ -4,6 +4,7 @@ def Calculate_profit_loss(selling_price,cost_price):
     if selling_price > cost_price:
         print("The profit is of rs.",selling_price - cost_price)
     elif selling_price == cost_price:
+        
         print("No loss no profit")
     else:
         print("The loss is of rs.",cost_price - selling_price)      
@@ -15,4 +16,4 @@ try:
     Calculate_profit_loss(n,m)
 
 except ValueError:
-    print("print the valid number of prices.")
+    print("please enter the valid number of prices.")
