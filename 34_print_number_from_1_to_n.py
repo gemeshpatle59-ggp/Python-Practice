592fe0a1d4b5faa0b7e5c38a4a01aa296ffd0dea
Gemesh patle <gemeshpatle59@gmail.com>
2026-08-15 22:08:13 +0530

    Add program 24 - Print number from 1 to n
     A simple python program to print the number from 1 to n by using head resursion.

---
+++ 34_print_number_from_1_to_n.py
@@ -0,0 +1,15 @@
+# Print numbers from 1 to N
+
+try:
+    n = int(input("Enter the number here.: "))
+
+    def num(n):
+        if n == 0:
+            return 
+        num(n-1)
+        print(n)
+
+    num(n)    
+
+except ValueError:
+    print("Please enter the valid number in input.")
