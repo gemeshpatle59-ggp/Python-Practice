a2305e6d8a70d37d20b84a7d7aaf592f282938d6
Gemesh patle <gemeshpatle59@gmail.com>
2026-08-19 18:56:50 +0530

    Add program 51 - find the first digit

---
+++ 51_find_the_first_digit_of_number.py
@@ -0,0 +1,15 @@
+# Find the last digit of a number
+
+def len_num():
+    try:
+        num = int(input("Enter the number here.: "))
+        n = len(str(num))
+        last_digit = n - 1
+        print(f"The first digit of number is {num // 10**last_digit}")
+
+
+    except ValueError:
+        print("Plaese enter the valid number.")
+
+if __name__ == "__main__":
+    len_num()        
